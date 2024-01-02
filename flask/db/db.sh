3819e653e17ab56f7b672d1622e08d864f725d9c
YenChou <chou950819@gmail.com>
2024-01-02 11:23:06 +0800

    [Yen] Add DB Script to replace schena

---
+++ flask/db/db.sh
@@ -0,0 +1,8 @@
+docker run --name flask_db -p 5432:5432 -e POSTGRES_PASSWORD=123456 -d postgres -c shared_buffers=2048MB -c max_connections=1000
+docker exec flask_db psql -U postgres -c "create database alter"
+docker exec flask_db psql -U postgres -c "CREATE TABLE if not exists "alterData" (
+    "No" serial4 not null primary key,
+    "UUID" varchar not null,
+    "Temperature" int4,
+    "CreateDt" timestamp with time zone default NOW()
+);"
--- flask/db/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE if not exists "alterData" (
-    "No" serial4 not null primary key,
-    "UUID" varchar not null,
-    "Temperature" int4,
-    "CreateDt" timestamp with time zone default NOW()
-);
