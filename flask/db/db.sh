56ec4154423c9cca8b975b6cb8f87134abcdd5c1
Jude <45480093+JudeTe@users.noreply.github.com>
2024-01-02 18:41:38 +0800

    Fix Flask shell

--- flask/db/db.sh
+++ flask/db/db.sh
@@ -1,4 +1,5 @@
-docker run --name flask_db -p 5432:5432 -e POSTGRES_PASSWORD=123456 -d postgres -c shared_buffers=2048MB -c max_connections=1000
+docker run --name flask_db -p 5432:5432 -e POSTGRES_PASSWORD=123456 -d postgres:16-bullseye -c shared_buffers=2048MB -c max_connections=1000
+sleep 5
 docker exec flask_db psql -U postgres -c "create database alter"
 docker exec flask_db psql -U postgres -c "CREATE TABLE if not exists "alterData" (
     "No" serial4 not null primary key,
